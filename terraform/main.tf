7529367a059f6d015ecf89dbace65c047d746fb1
Medhansh-Xplores <me23btech11046@iith.ac.in>
2026-01-30 07:37:13 +0000

    AFT: request sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,22 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "medhanshpendem+sandbox@gmail.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    ManagedOrganizationalUnit = "Sandbox"
+
+    SSOUserEmail     = "mpendam@autonomicsolutionsllc.com"
+    SSOUserFirstName = "Medhansh"
+    SSOUserLastName  = "Pendem"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    Environment = "sandbox"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "AFT"
+    change_reason       = "Sandbox account via AFT"
   }
 
   custom_fields = {
